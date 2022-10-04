fe99536b8a7a4e433e9b135b83f35a5c283975c5
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-04 20:48:34 -0300

    add class hour contract

---
+++ OOPEnumCOmposition/Entities/HourContract.cs
@@ -0,0 +1,30 @@
+﻿
+namespace OOPEnumComposition.Entities
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; set; }
+
+        public double ValuePerHour { get; set; }
+
+        public int Hour { get; set; }
+
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime data, double valuePerHour, int hour)
+        {
+            Date = data;
+            ValuePerHour = valuePerHour;
+            Hour = hour;
+        }
+
+
+        public  double TotalValue()
+        {
+            return ValuePerHour * Hour;
+        }
+    }
+}
