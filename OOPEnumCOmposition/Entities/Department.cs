72cec0aaf067ca7a1606c2f635a7659c3da0422f
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-04 20:37:26 -0300

    add class department

---
+++ OOPEnumCOmposition/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace OOPEnumComposition.Entities
+{
+    internal class Department
+    {
+      
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
